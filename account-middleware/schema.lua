b21f9189c47d510fc99d826551bccac379015381
SyaibanAhmadRamadhan <ibanrama29@gmail.com>
2023-09-22 19:55:14 +0700

    fix account middleware

--- account-middleware/schema.lua
+++ account-middleware/schema.lua
@@ -1,7 +1,7 @@
 local typedefs = require "kong.db.schema.typedefs"
 
 return {
-    name = "custom-auth",
+    name = "account-middleware",
     fields = {
         { protocols = typedefs.protocols_http },
         { config = {
